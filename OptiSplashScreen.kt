2fb32cee579b173b45585501f95a375f64dcdc8d
Vineet Singh Rawat <60997111+vineet767@users.noreply.github.com>
2022-05-23 13:11:07 +0530

    Add files via upload

---
+++ OptiSplashScreen.kt
@@ -0,0 +1,33 @@
+
+
+package com.obs.marveleditor
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.support.v7.app.AppCompatActivity
+
+class OptiSplashScreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.opti_splash_screen)
+        initListener()
+    }
+
+    private fun initListener() {
+        val handler = Handler()
+        handler.postDelayed({ callActivityIntent() }, 3000)
+    }
+
+    private fun callActivityIntent() {
+        val intentFlag = Intent(this@OptiSplashScreen, MainActivity::class.java)
+        intentFlag.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        startActivity(intentFlag)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        finish()
+    }
+}
